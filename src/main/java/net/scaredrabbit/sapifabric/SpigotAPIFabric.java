3c349b2928fefe015238010d0b6fd50048ed73b7
BuildTools <unconfigured@null.spigotmc.org>
2023-07-06 19:03:11 +0200

    Initial commit

---
+++ src/main/java/net/scaredrabbit/sapifabric/SpigotAPIFabric.java
@@ -0,0 +1,23 @@
+package net.scaredrabbit.sapifabric;
+
+import net.fabricmc.api.ModInitializer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotAPIFabric extends JavaPlugin implements ModInitializer {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
