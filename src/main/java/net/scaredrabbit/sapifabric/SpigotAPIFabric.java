6c1581b54e543321502ea52997b167121ccd0a31
ScaredRabbit <103259379+ScaredRabbitYT@users.noreply.github.com>
2023-09-19 14:50:46 +0200

    Optimized main class

--- src/main/java/net/scaredrabbit/sapifabric/SpigotAPIFabric.java
+++ src/main/java/net/scaredrabbit/sapifabric/SpigotAPIFabric.java
@@ -3,18 +3,7 @@ package net.scaredrabbit.sapifabric;
 import net.fabricmc.api.ModInitializer;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SpigotAPIFabric extends JavaPlugin implements ModInitializer {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+public final class SpigotAPIFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
